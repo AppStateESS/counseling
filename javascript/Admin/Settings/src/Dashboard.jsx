c11c2c3a0c224292ec0b7bd0e1ae3f627a2adc54
Stardog <mcnaney@gmail.com>
2015-11-04 12:35:45 -0500

    Moved TextInput to Mixins file

--- javascript/Admin/Settings/src/Dashboard.jsx
+++ javascript/Admin/Settings/src/Dashboard.jsx
@@ -52,6 +52,8 @@ var Tab = React.createClass({
 });
 
 var Content = React.createClass({
+    mixins : ['TextInput'],
+
     getInitialState: function() {
         return {
         };
@@ -111,54 +113,5 @@ var BoolIcon = React.createClass({
     }
 });
 
-
-var TextInput = React.createClass({
-    getDefaultProps: function() {
-        return {
-            label: '',
-            placeholder: '',
-            handleBlur:null,
-            required: false,
-            handlePress : null,
-            handleChange : null,
-            inputId : null,
-            defaultValue : null
-        };
-    },
-
-    handleBlur : function(e) {
-        if (this.props.required && e.target.value.length < 1) {
-            $(e.target).css('border-color', 'red');
-        }
-        if (this.props.handleBlur) {
-            this.props.handleBlur(e);
-        }
-    },
-
-    handleFocus : function(e) {
-        $(e.target).css('border-color', '');
-    },
-
-    render : function() {
-        var label = '';
-        var required = '';
-        if (this.props.label.length > 0) {
-            if (this.props.required) {
-                required = <i className="fa fa-asterisk text-danger"></i>;
-            }
-            label = <label htmlFor={this.props.inputId}>{this.props.label}</label>;
-        } else {
-            label = null;
-        }
-        return (
-            <div className="form-group">
-                {label} {required}
-                <input type="text" className="form-control" id={this.props.inputId}
-                    name={this.props.inputId} placeholder={this.props.placeholder} onFocus={this.handleFocus}
-                    onChange={this.props.handleChange} onBlur={this.handleBlur} onKeyPress={this.props.handlePress} defaultValue={this.props.defaultValue}/>
-            </div>
-        );
-    }
-});
 
 ReactDOM.render(<SettingsDashboard/>, document.getElementById('settings-dashboard'));
