d0787558ba7e4afbe4b1c286a61ecfece667d966
Stardog <mcnaney@gmail.com>
2015-11-04 12:59:36 -0500

    Fleshed out the phone sign in screen

--- javascript/User/Checkin/src/Phone.jsx
+++ javascript/User/Checkin/src/Phone.jsx
@@ -1,18 +1,71 @@
 var Phone = React.createClass({
+    mixins: [errorTimeout],
+
     getInitialState: function() {
         return {
+            phoneNumber : '',
+            error : false
         };
     },
 
     getDefaultProps: function() {
         return {
+            update : null,
+            visitor : null
         };
     },
 
+    componentDidMount : function() {
+        this.setState({
+            phoneNumber : this.props.visitor.phoneNumber
+        });
+    },
+
+    updatePhone : function() {
+        if (this.state.phoneNumber.length < 10) {
+            this.setState({error:true});
+        } else {
+            this.props.update(this.state.phoneNumber);
+        }
+    },
+
+    handleChange : function(e) {
+        this.setState({
+            phoneNumber : e.target.value
+        });
+    },
+
     render: function() {
-        return (
-            <div />
+        var field = null;
+        if (this.state.error) {
+            field = (
+                <div className="alert alert-danger alert-dismissible" role="alert" ref="errorAlert">
+                    <button className="close" type="button" onClick={this.resetForm}><i className="fa fa-times"></i></button>
+                    Please enter your cell phone number including your area code.
+                </div>
+            );
+        } else {
+            field = <input type="text" ref="phone" className="form-control" placeholder="Cell phone number with area code"
+                value={this.state.phoneNumber} onChange={this.handleChange}/>
+        }
+
+        var content =  (
+            <div>
+                <div className="text-center">
+                    <p className="title">Ok, {this.props.visitor.firstName}.</p>
+                    <p className="subtitle">In case we need to reach you later,<br />
+                        please enter your cell phone number.</p>
+                    {field}
+                    <button className="continue pull-right btn btn-default" onClick={this.updatePhone}>Continue <i className="fa fa-chevron-right fa-sm"></i></button>
+                    <div className="clearfix"></div>
+                </div>
+            </div>
         );
+
+        return (
+            <Box content={content}/>
+        );
+
     }
 
 });
