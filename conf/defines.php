fd0b356a95995fefe335292f37173ab6a366d2e8
Stardog <mcnaney@gmail.com>
2015-11-04 12:49:24 -0500

    Added defines for the instructions

--- conf/defines.php
+++ conf/defines.php
@@ -8,3 +8,6 @@
 define('COUNSELING_REACT_DEV', true);
 
 define('COUNSELING_FAKE_STUDENT', true);
+
+define('COUNSELING_SIT_INSTRUCTION', 'Please have a seat in the lobby area. One of our counselors will speak with you shortly.');
+define('COUNSELING_FRONT_DESK_INSTRUCTION', 'Please see one of our staff members at the front desk for your next steps.');
