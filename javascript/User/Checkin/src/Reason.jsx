6ea37b4e11f59b3393855f4ab7cd29e4daad13d6
Stardog <mcnaney@gmail.com>
2015-11-04 12:59:53 -0500

    Fleshed out reason sign in ui

--- javascript/User/Checkin/src/Reason.jsx
+++ javascript/User/Checkin/src/Reason.jsx
@@ -1,18 +1,58 @@
 var Reason = React.createClass({
     getInitialState: function() {
         return {
+            reasons : null,
         };
     },
 
     getDefaultProps: function() {
         return {
-
+            update: null,  // function to run upon reason selection
+            visitor : null, //visitor object
+            update : null // function to set parent's reason
         };
     },
 
+    componentDidMount: function() {
+        $.getJSON('counseling/User/Reason', {
+        	command : 'list'
+        }).done(function(data){
+            this.setState({
+                reasons : data
+            })
+        }.bind(this));
+
+    },
+
+    pickReason : function(key)
+    {
+        this.props.update(this.state.reasons[key]);
+    },
+
     render: function() {
+        var content;
+
+        if (this.state.reasons === null || this.state.reasons.length === 0 ){
+            content = <div className="alert alert-danger">System error: Please alert front desk.</div>;
+        } else {
+            var reasonList = this.state.reasons.map(function(value, i){
+                return <li key={i} className="list-group-item pointer" onClick={this.pickReason.bind(this, i)}>{value.description}</li>;
+            }.bind(this));
+            content = (
+                <div>
+                    <div className="text-center">
+                        <p className="title">Hello, {this.props.visitor.firstName}.</p>
+                        <p className="title">Why are you visiting today?</p>
+                    </div>
+                    <ul className="list-group">
+                        {reasonList}
+                    </ul>
+                </div>
+            );
+        }
+
         return (
-            <div>Reason</div>
+            <Box content={content}/>
         );
     }
 
