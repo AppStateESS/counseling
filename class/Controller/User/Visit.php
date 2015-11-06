e9e4b8ca46db4ac69e756df7a690569a224fc63f
Stardog <mcnaney@gmail.com>
2015-11-06 15:41:38 -0500

    New controller class to handle User Visit requests

---
+++ class/Controller/User/Visit.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace counseling\Controller\User;
+
+use counseling\Factory\Visit as Factory;
+use counseling\Resource\Visit as Resource;
+
+/**
+ * @license http://opensource.org/licenses/lgpl-3.0.html
+ * @author Matthew McNaney <mcnaney at gmail dot com>
+ */
+class Visit extends \counseling\Controller\Base
+{
+
+    public function post(\Request $request)
+    {
+        if (!$request->isVar('command')) {
+            throw new \Exception('Unknown Reason command');
+        }
+
+        $command = $request->getVar('command');
+        switch ($command) {
+            case 'create':
+                $this->create();
+                break;
+        }
+        $view = new \View\JsonView(array('success' => true));
+        $response = new \Response($view);
+        return $response;
+    }
+    
+    private function create()
+    {
+        $visitor_id = Factory::pullPostInteger('visitorId');
+        $reason_id = Factory::pullPostInteger('reasonId');
+        $emergency = Factory::pullPostCheck('emergency');
+        
+        $visit = new Resource;
+        $visit->stampArrivalTime();
+        $visit->setHasEmergency($emergency);
+        $visit->setReasonId($reason_id);
+        $visit->setVisitorId($visitor_id);
+        Factory::saveResource($visit);
+    }
+
+}
