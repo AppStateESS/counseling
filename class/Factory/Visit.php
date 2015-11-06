9ad2a6bd6960c338f0b70eeea2b32fd169a2d271
Stardog <mcnaney@gmail.com>
2015-11-06 15:53:16 -0500

    New factory.

---
+++ class/Factory/Visit.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace counseling\Factory;
+
+use counseling\Resource\Visit as Resource;
+
+/**
+ * @license http://opensource.org/licenses/lgpl-3.0.html
+ * @author Matthew McNaney <mcnaney at gmail dot com>
+ */
+class Visit extends Base
+{
+    
+}
