3efcd67fe7b8c034e42b987c3e4ed9b5c1c62a31
Stardog <mcnaney@gmail.com>
2015-11-10 14:02:09 -0500

    Removed settings button. Moved emergency to own file.

--- javascript/Admin/FrontDesk/src/Dashboard.jsx
+++ javascript/Admin/FrontDesk/src/Dashboard.jsx
@@ -6,29 +6,10 @@ var Dashboard = React.createClass({
 
     render: function() {
         return (
-            <div>
-                {settingsAllowed ? <a href="counseling/Admin/Settings" className="btn btn-default"><i className="fa fa-gear"></i> Settings</a> : null}
+            <div className="dashboard">
                 <Summary />
-                <Emergency />
                 <Waiting />
             </div>
-        );
-    }
-
-});
-
-var Emergency = React.createClass({
-    getInitialState: function() {
-        return {};
-    },
-
-    getDefaultProps: function() {
-        return {};
-    },
-
-    render: function() {
-        return (
-            <div>Emergency here</div>
         );
     }
 
