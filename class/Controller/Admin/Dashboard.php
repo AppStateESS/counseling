e6492217e20ddd41990a80c8de1f6c2407177eaa
Stardog <mcnaney@gmail.com>
2015-11-10 14:01:19 -0500

    New controls to delegate commands.

--- class/Controller/Admin/Dashboard.php
+++ class/Controller/Admin/Dashboard.php
@@ -8,22 +8,64 @@ namespace counseling\Controller\Admin;
  */
 class Dashboard extends \counseling\Controller\Base
 {
+
+    public function get(\Request $request)
+    {
+        $command = $this->routeCommand($request);
+        if (empty($command)) {
+            return parent::get($request);
+        } else {
+            return $command->get($request);
+        }
+    }
+
+    public function post(\Request $request)
+    {
+        $command = $this->routeCommand($request);
+        if (empty($command)) {
+            return parent::post($request);
+        } else {
+            return $command->post($request);
+        }
+    }
+
     public function getHtmlView($data, \Request $request)
     {
+        
         if (COUNSELING_REACT_DEV) {
+            \counseling\Factory\React::load('Admin/FrontDesk/', 'Mixins.jsx');
             \counseling\Factory\React::load('Admin/FrontDesk/', 'Summary.jsx');
+            \counseling\Factory\React::load('Admin/FrontDesk/', 'Emergency.jsx');
             \counseling\Factory\React::load('Admin/FrontDesk/', 'Waiting.jsx');
             \counseling\Factory\React::load('Admin/FrontDesk/', 'Dashboard.jsx');
         }
+
+        \Layout::addStyle('counseling', 'Admin/Dashboard/style.css');
         
         $settings = \Current_User::isDeity() ? 'true' : 'false';
-        
+
         $content = <<<EOF
 <script type="text/javascript">var settingsAllowed = $settings;</script>
 <div id="dashboard"></div>
 EOF;
         $view = new \View\HtmlView($content);
         return $view;
+    }
+
+    private function routeCommand($request)
+    {
+        $command = $request->shiftCommand();
+
+        if (empty($command)) {
+            return null;
+        }
+
+        $className = 'counseling\Controller\Admin\Dashboard\\' . $command;
+        if (!class_exists($className)) {
+            throw new \Http\NotAcceptableException($request);
+        }
+        $commandObject = new $className($this->getModule());
+        return $commandObject;
     }
 
 }
