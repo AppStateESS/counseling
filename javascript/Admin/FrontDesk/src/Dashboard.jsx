b5b4a2712c048e3b16e619d5b9794ee321d42d1a
Stardog <mcnaney@gmail.com>
2015-11-16 14:00:26 -0500

    moved data higher into the Dashboard and passing it down to summary and waiting.

--- javascript/Admin/FrontDesk/src/Dashboard.jsx
+++ javascript/Admin/FrontDesk/src/Dashboard.jsx
@@ -1,9 +1,33 @@
 var Dashboard = React.createClass({
+    getInitialState: function() {
+        return {
+            emergencyList : null,
+            waitingList : null,
+            summary : null
+        };
+    },
+
+    componentDidMount : function() {
+        this.loadData();
+    },
+
+    loadData : function() {
+        $.getJSON('counseling/Admin/Dashboard/Waiting', {
+        	command : 'list'
+        }).done(function(data){
+            this.setState({
+                emergencyList : data.emergencies,
+                waitingList : data.waiting,
+                summary : data.summary
+            });
+        }.bind(this));
+    },
+
     render: function() {
         return (
             <div className="dashboard">
-                <Summary />
-                <Waiting />
+                <Summary data={this.state.summary} reload={this.loadData}/>
+                <Waiting emergency={this.state.emergencyList} waiting={this.state.waitingList} reload={this.loadData}/>
             </div>
         );
     }
