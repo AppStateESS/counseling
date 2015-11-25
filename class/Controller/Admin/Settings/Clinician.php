463c59bbf582d9cae062aa3895b4b5c235cf50e8
Stardog <mcnaney@gmail.com>
2015-11-25 13:35:41 -0500

    New class to create and edit clinicians.

---
+++ class/Controller/Admin/Settings/Clinician.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace counseling\Controller\Admin\Settings;
+
+use counseling\Factory\Clinician as Factory;
+
+/**
+ * @license http://opensource.org/licenses/lgpl-3.0.html
+ * @author Matthew McNaney <mcnaney at gmail dot com>
+ */
+class Clinician extends \counseling\Controller\Base
+{
+
+    protected function getJsonView($data, \Request $request)
+    {
+        if (!$request->isVar('command')) {
+            throw new \Exception('Unknown Settings command');
+        }
+        $json = array('success' => true);
+
+        $command = $request->getVar('command');
+        switch ($command) {
+            case 'list':
+                $json = Factory::getList();
+                break;
+        }
+        $view = new \View\JsonView($json);
+        return $view;
+    }
+
+    public function post(\Request $request)
+    {
+        if (!$request->isVar('command')) {
+            throw new \Exception('Unknown Clinician command');
+        }
+
+        $command = $request->getVar('command');
+        switch ($command) {
+            case 'save':
+                Factory::post();
+                break;
+
+            case 'delete':
+                Factory::delete(Factory::pullPostInteger('clinicianId'));
+                break;
+            
+            default:
+            throw new \Exception('Unknown Clinician command');
+        }
+
+        $view = new \View\JsonView(array('success' => true));
+        $response = new \Response($view);
+        return $response;
+    }
+}
