24d651f6b768014728e44c5a3cc7f0dbee4dda44
Stardog <mcnaney@gmail.com>
2015-12-07 13:46:26 -0500

    New factory for Disposition.

---
+++ class/Factory/Disposition.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace counseling\Factory;
+
+use counseling\Resource\Disposition as Resource;
+
+/**
+ * @license http://opensource.org/licenses/lgpl-3.0.html
+ * @author Matthew McNaney <mcnaney at gmail dot com>
+ */
+class Disposition extends Base
+{
+
+    public static function getList($active_only = true)
+    {
+        $db = \Database::getDB();
+        $tbl = $db->addTable('cc_disposition');
+        $tbl->addFieldConditional('active', 1);
+        $tbl->addOrderBy('title');
+        return $db->select();
+    }
+
+    public static function post()
+    {
+        $disposition_id = filter_input(INPUT_POST, 'dispositionId', FILTER_SANITIZE_NUMBER_INT);
+        $disposition = self::build($disposition_id);
+        $disposition->setTitle(self::pullPostString('title'));
+
+        self::saveResource($disposition);
+    }
+
+    public static function build($id = 0)
+    {
+        $disposition = new Resource;
+        if ($id) {
+            $disposition->setId($id);
+            if (!parent::loadByID($disposition)) {
+                throw new \Exception('Disposition id not found:' . $id);
+            }
+        }
+        return $disposition;
+    }
+
+    public static function delete($id)
+    {
+        $disposition = self::build($id);
+        $disposition->setActive(false);
+        self::saveResource($disposition);
+    }
+
+}
