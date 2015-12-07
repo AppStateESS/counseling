1f5b507238788267e207b2eeb21cbf7a92aa9451
Stardog <mcnaney@gmail.com>
2015-12-07 13:45:45 -0500

    Added card swipe code.

--- javascript/User/Checkin/src/Swipe.jsx
+++ javascript/User/Checkin/src/Swipe.jsx
@@ -1,11 +1,12 @@
 var ReactCSSTransitionGroup = React.addons.CSSTransitionGroup;
+var swipeTimeout = null;
 
 var Swipe = React.createClass({
     mixins: [errorTimeout],
 
     getInitialState: function() {
         return {
-            error : false,
+            error : 0,
             visitor : null
         };
 
@@ -20,22 +21,55 @@ var Swipe = React.createClass({
     loginFailure : function() {
         this.setState({
             visitor : '',
-            error : true
+            error : 1
+        });
+        this.timedReset();
+    },
+
+    alreadyVisiting : function() {
+        this.setState({
+            visitor : '',
+            error : 2
+        });
+    },
+
+    timedReset : function() {
+        swipeTimeout = setTimeout(function(){
+            this.resetSwipe();
+        }.bind(this), 5000);
+    },
+
+    resetSwipe : function() {
+        this.setState({
+            error : 0,
+            visitor : null,
+            swipeStarted : false
         });
     },
 
     logInVisitor : function() {
-        if (this.state.visitor && this.state.visitor.length > 3) {
+        var visitor = this.state.visitor;
+        if (visitor && visitor.length > 3) {
+            if (visitor.charAt(0) === ';' && visitor.charAt(10) === '=') {
+                visitor = visitor.slice(1,10);
+            }
+
+            console.log(visitor);
             $.getJSON('counseling/User/Checkin', {
             	command : 'loginVisitor',
-                bannerId : this.state.visitor
-            }).done(function(data){
-                if (data.visitor === null) {
+                bannerId : visitor
+            })
+            .done(function(data){
+                if(data.waiting !== undefined) {
+                    this.alreadyVisiting();
+                    this.timedReset();
+                } else if (data.visitor === null) {
                     this.loginFailure();
                 } else {
                     this.props.update(data);
                 }
-            }.bind(this)).fail(function() {
+            }.bind(this))
+            .fail(function() {
                 this.loginFailure();
             }.bind(this));
         } else {
@@ -44,25 +78,53 @@ var Swipe = React.createClass({
     },
 
     handleChange : function(e) {
+        var character = e.target.value;
         this.setState({
-            visitor : e.target.value
+            visitor : character
         });
+    },
+
+    submitVisitor : function(e)
+    {
+        e.preventDefault();
+        this.logInVisitor();
+    },
+
+    focusSwiper : function() {
+        $('#swiper').focus();
+    },
+
+    componentDidUpdate: function(prevProps, prevState) {
+        this.focusSwiper();
+    },
+    
+    componentDidMount: function(prevProps, prevState) {
+        this.focusSwiper();
     },
 
     render: function() {
         var field = null;
+        var button = null;
 
-        if (this.state.error) {
+        if (this.state.error === 1) {
             field = (
                 <div className="alert alert-danger alert-dismissible" role="alert" ref="errorAlert">
-                    <button className="close" type="button" onClick={this.resetForm}><i className="fa fa-times"></i></button>
+                    <button className="close" type="button" onClick={this.resetSwipe}><i className="fa fa-times"></i></button>
                     Account not found. Please try again or see the front desk.
                 </div>
             );
+        } else if (this.state.error === 2) {
+            field = (
+                <div className="alert alert-warning alert-dismissible" role="alert" ref="errorAlert">
+                    <button className="close" type="button" onClick={this.resetSwipe}><i className="fa fa-times"></i></button>
+                    You are already logged in. Please visit the front desk if you have a question or concern.
+                </div>
+            );
         } else {
-            field = <input type="text" placeholder="Banner ID" onChange={this.handleChange} className="form-control" value={this.state.visitor} />;
+            field = <input id="swiper" type="text" placeholder="Banner ID" onChange={this.handleChange}
+                 className="form-control" value={this.state.visitor} />;
+            button = <button className="continue pull-right btn btn-default" onClick={this.logInVisitor}>Continue <i className="fa fa-chevron-right fa-sm"></i></button>;
         }
-
 
         var content = (
             <div>
@@ -70,9 +132,11 @@ var Swipe = React.createClass({
                     <p className="title">Welcome! Please Check-in</p>
                     <p className="subtitle">Swipe your AppCard to get started</p>
                     <p>Don't have your AppCard?<br />Enter your Banner ID number instead.</p>
+                    <form onSubmit={this.submitVisitor}>
                     {field}
+                    </form>
                 </div>
-                <button className="continue pull-right btn btn-default" onClick={this.logInVisitor}>Continue <i className="fa fa-chevron-right fa-sm"></i></button>
+                {button}
                 <div className="clearfix"></div>
             </div>
         );
