66fce69a1f6a9e02591c77d34fbac2a84ff8e11b
Stardog <mcnaney@gmail.com>
2015-12-10 16:10:52 -0500

    added more options to report factory.

--- class/Factory/Report.php
+++ class/Factory/Report.php
@@ -9,19 +9,38 @@ namespace counseling\Factory;
 class Report extends Base
 {
 
-    public static function view()
+    public static function daily(\Request $request)
     {
-        $visits = Visit::getTodaysVisits();
+        javascript('datepicker');
+        $script = '<script type="text/javascript" src="' . PHPWS_HOME_HTTP . 'mod/counseling/javascript/Admin/Report/script.js"></script>';
+        
+        $datestamp = $request->shiftCommand();
+        if (empty($datestamp)) {
+            $start_time = self::getTodayStartTime();
+            $end_time = self::getTodayEndTime();
+        } else {
+            $start_time = strtotime($datestamp);
+            $end_time = mktime(23, 59, 59, date('n', $start_time), date('j', $start_time), date('Y', $start_time));
+        }
+        
+        \Layout::addJSHeader($script);
+        
+        $visits = Visit::getDaysVisits($start_time, $end_time);
         $seen = array();
         $unseen = array();
 
-        foreach ($visits as $visit) {
-            if ($visit['complete_reason'] == CC_COMPLETE_SEEN) {
-                $vars['seen'][] = $visit;
-            } else {
-                $vars['unseen'][] = $visit;
+        if (!empty($visits)) {
+            foreach ($visits as $visit) {
+                if ($visit['complete_reason'] == CC_COMPLETE_SEEN) {
+                    $vars['seen'][] = $visit;
+                } else {
+                    $vars['unseen'][] = $visit;
+                }
             }
         }
+        $vars['date'] = strftime('%a, %B %e, %Y', $start_time);
+        $vars['startTime'] = strftime('%Y%m%d', $start_time);
+                
         $template = new \Template($vars);
         $template->setModuleTemplate('counseling', 'Admin/Report/daily.html');
         return $template->get();
