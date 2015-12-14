a69f94ccf0f1772d207a5c83e37ef20466f1cbdf
Stardog <mcnaney@gmail.com>
2015-12-14 13:17:31 -0500

    Moved defines that need setting to top.

--- conf/defines.dist.php
+++ conf/defines.dist.php
@@ -7,13 +7,18 @@
 
 define('COUNSELING_DEFINE_VERSION', '1.0');
 
-define('COUNSELING_REACT_DEV', true);
+define('COUNSELING_REACT_DEV', false);
 
-define('COUNSELING_FAKE_VISITOR', true);
+define('COUNSELING_FAKE_VISITOR', false);
+
+define('COUNSELING_BANNER_URL', 'http://url.to.banner.api/');
+
+define('COUNSELING_TEMP_FOLDER', '/tmp/');
+
+/** Defines below do not need editing **/
 
 define('COUNSELING_SIT_INSTRUCTION', 'Please have a seat in the lobby area. One of our counselors will speak with you shortly.');
 define('COUNSELING_FRONT_DESK_INSTRUCTION', 'Please see one of our staff members at the front desk for your next step.');
-
 
 define('CC_CATEGORY_OTHER', 0);
 define('CC_CATEGORY_WALKIN', 1);
@@ -31,7 +36,3 @@ define('CC_COMPLETE_SEEN', 1);
 define('CC_COMPLETE_LEFT', 2);
 define('CC_COMPLETE_MISSING', 3);
 define('CC_COMPLETE_APPOINTMENT', 4);
-
-define('COUNSELING_TEMP_FOLDER', '/tmp/');
-
-define('COUNSELING_BANNER_URL', 'http://url.to.banner.api/');
