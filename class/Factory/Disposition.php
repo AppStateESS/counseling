1953f8f850bb4b8477e9bcba06547d29271cf9ec
Stardog <mcnaney@gmail.com>
2015-12-14 13:16:49 -0500

    Fixed some flaws in sorting.

--- class/Factory/Disposition.php
+++ class/Factory/Disposition.php
@@ -27,6 +27,9 @@ class Disposition extends Base
         $disposition->setTitle(self::pullPostString('title'));
         $disposition->setIcon(self::pullPostString('icon'));
         $disposition->setColor(self::pullPostString('color'));
+        if (empty($disposition_id)) {
+            $disposition->setSorting(self::countDispositions() + 1);
+        }
 
         self::saveResource($disposition);
     }
@@ -50,32 +53,42 @@ class Disposition extends Base
         $disposition->setSorting(0);
         self::saveResource($disposition);
     }
-    
+
+    private static function countDispositions()
+    {
+        $db = \Database::getDB();
+        $tbl = $db->addTable('cc_disposition');
+        $tbl->addFieldConditional('active', 1);
+        $tbl->addField(new \Database\Expression('count(' . $tbl->getField('id') . ')', 'count'));
+        $count = $db->selectColumn();
+        return $count;
+    }
+
     public static function sort($moved_id, $prev_id, $next_id)
     {
         // decrement sorting number of all previous up to moved
-        
+
         $moved_obj = self::build($moved_id);
         $moved_sort = $moved_obj->getSorting();
-        
+
         if (!empty($prev_id)) {
             $prev_obj = self::build($prev_id);
             $prev_sort = $prev_obj->getSorting();
         } else {
-            $prev_sort = 0;
+            $prev_sort = 1;
         }
 
         if (!empty($next_id)) {
             $next_obj = self::build($next_id);
             $next_sort = $next_obj->getSorting();
         } else {
-            $next_sort = 999;
+            $next_sort = self::countDispositions();
         }
 
         $db = \Database::getDB();
         $tbl = $db->addTable('cc_disposition');
         $tbl->addFieldConditional('active', 1);
-        
+
         if ($moved_sort > $prev_sort) {
             // moved downward, increase all above
             $tbl->addFieldConditional('sorting', $next_sort, '>=');
@@ -89,10 +102,11 @@ class Disposition extends Base
             $exp = $db->getExpression('sorting - 1');
             $moved_obj->setSorting($prev_sort);
         }
-        
+
         $tbl->addValue('sorting', $exp);
         $db->update();
 
         self::saveResource($moved_obj);
     }
+
 }
