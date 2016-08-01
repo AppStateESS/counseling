ab41a26c4861bb24e50c473bf2e3b7d6a7a04f11
Matthew McNaney <mcnaney@gmail.com>
2016-08-01 09:48:10 -0400

    All visits are now recorded
    Going to show appointment visits.

--- class/Controller/User/Visit.php
+++ class/Controller/User/Visit.php
@@ -11,7 +11,6 @@ use counseling\Resource\Visit as Resource;
  */
 class Visit extends \counseling\Controller\Base
 {
-
     public function post(\Request $request)
     {
         if (!$request->isVar('command')) {
@@ -26,24 +25,23 @@ class Visit extends \counseling\Controller\Base
         }
         $view = new \View\JsonView(array('success' => true));
         $response = new \Response($view);
+
         return $response;
     }
-    
+
     private function create()
     {
         $visitor_id = Factory::pullPostInteger('visitorId');
         $reason_id = Factory::pullPostInteger('reasonId');
         $emergency = Factory::pullPostCheck('emergency');
-        
-        $reason = \counseling\Factory\Reason::build($reason_id);
-        if ($reason->getWaitListed()) {
-            $visit = new Resource;
-            $visit->stampArrivalTime();
-            $visit->setHasEmergency($emergency);
-            $visit->setReasonId($reason_id);
-            $visit->setVisitorId($visitor_id);
-            Factory::saveResource($visit);
-        }
-    }
 
+        $reason = \counseling\Factory\Reason::build($reason_id);
+        $visit = new Resource();
+        $visit->stampArrivalTime();
+        $visit->setHasEmergency($emergency);
+        $visit->setReasonId($reason_id);
+        $visit->setVisitorId($visitor_id);
+        $visit->setWaiting($reason->getWaitListed());
+        Factory::saveResource($visit);
+    }
 }
