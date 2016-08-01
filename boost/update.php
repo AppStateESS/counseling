0cd536fb0405c374b7b3c62f0f87f7fa5d53163c
Matthew McNaney <mcnaney@gmail.com>
2016-08-01 09:46:13 -0400

    Adding waiting flag on visits

--- boost/update.php
+++ boost/update.php
@@ -13,6 +13,24 @@ function counseling_update(&$content, $version)
             $dt->add();
             $tbl->addValue('color', 'default');
             $db->update();
+            $content[] = <<<EOF
+<pre>2.0.0
+--------------
++ Added color highlighting to reasons
++ Disposition color type changed to match reason's
+</pre>
+EOF;
+        case version_compare($version, '2.1.0', '<'):
+            $db = \phpws2\Database::getDB();
+            $tbl = $db->addTable('cc_visit');
+            $dt = new \phpws2\Database\Datatype\Smallint($tbl, 'waiting');
+            $dt->add();
+            $content[] = <<<EOF
+<pre>2.1.0
+--------------
++ Adding 'waiting' flag to visits
+</pre>
+EOF;
     }
 
     return true;
