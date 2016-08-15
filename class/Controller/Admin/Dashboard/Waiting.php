82c6ad464176a1a80dccb586b6e23b159900f0c2
Matthew McNaney <mcnaney@gmail.com>
2016-08-15 12:11:04 -0400

    Fixed waiting data line removal.

--- class/Controller/Admin/Dashboard/Waiting.php
+++ class/Controller/Admin/Dashboard/Waiting.php
@@ -102,6 +102,7 @@ class Waiting extends \counseling\Controller\Base
                     $json['emergencies'][] = $visit;
                     ++$emergencies;
                 } else {
+                    $json['waiting'][] = $visit;
                     switch ($visit['category']) {
                         case CC_CATEGORY_OTHER:
                             $other++;
