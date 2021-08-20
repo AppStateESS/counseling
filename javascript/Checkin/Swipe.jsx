2e57142ed890988b7ef4568aeab03f3b48dbef94
Matt McNaney <mcnaneym@appstate.edu>
2021-08-20 12:11:36 -0400

    Changed swipe method to send automatically at 9 characters.
    Only numbers are allowed now.

--- javascript/Checkin/Swipe.jsx
+++ javascript/Checkin/Swipe.jsx
@@ -2,7 +2,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import ErrorTimeout from './ErrorTimeout'
-//import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
 
 /* global $ */
 
@@ -13,7 +12,7 @@ class Swipe extends ErrorTimeout {
     super(props)
     this.state = {
       error: 0,
-      visitor: ''
+      visitor: '',
     }
     this.handleChange = this.handleChange.bind(this)
     this.logInVisitor = this.logInVisitor.bind(this)
@@ -22,18 +21,21 @@ class Swipe extends ErrorTimeout {
   }
 
   loginFailure() {
-    this.setState({visitor: '', error: 1,})
+    this.setState({visitor: '', error: 1})
     this.timedReset()
   }
 
   alreadyVisiting() {
-    this.setState({visitor: '', error: 2,})
+    this.setState({visitor: '', error: 2})
   }
 
   timedReset() {
-    swipeTimeout = setTimeout(function () {
-      this.resetSwipe()
-    }.bind(this), 4000)
+    swipeTimeout = setTimeout(
+      function () {
+        this.resetSwipe()
+      }.bind(this),
+      4000
+    )
   }
 
   resetSwipe() {
@@ -42,39 +44,46 @@ class Swipe extends ErrorTimeout {
   }
 
   logInVisitor() {
-    var visitor = this.state.visitor
-    if (visitor && visitor.length > 3) {
-      if (visitor.charAt(0) === ';' && visitor.charAt(10) === '=') {
-        visitor = visitor.slice(1, 10)
-      }
-
+    const visitor = this.state.visitor
+    if (visitor && visitor.length === 9) {
       $.getJSON('counseling/User/Checkin', {
         command: 'loginVisitor',
-        bannerId: visitor,
-      }).done(function (data) {
-        if (data.waiting !== undefined) {
-          this.alreadyVisiting()
-          this.timedReset()
-        } else if (data.visitor === null) {
-          this.loginFailure()
-        } else {
-          this.props.update(data)
-        }
-      }.bind(this)).fail(function () {
-        this.loginFailure()
-      }.bind(this))
+        studentBannerId: visitor,
+      })
+        .done(
+          function (data) {
+            if (data === null) {
+              this.loginFailure()
+            } else if (data.waiting !== undefined) {
+              this.alreadyVisiting()
+              this.timedReset()
+            } else if (data.visitor === null) {
+              this.loginFailure()
+            } else {
+              this.props.update(data)
+            }
+          }.bind(this)
+        )
+        .fail(
+          function () {
+            this.loginFailure()
+          }.bind(this)
+        )
     } else {
       this.loginFailure()
     }
   }
 
-  handleChange(e) {
-    var character = e.target.value
-    this.setState({visitor: character})
+  handleChange(value) {
+    if (value.length <= 9 && value.match(/^[\d\b]{0,9}$/)) {
+      this.setState({visitor: value})
+    }
+    if (value.length === 9) {
+      this.setState({visitor: value}, this.logInVisitor)
+    }
   }
 
-  submitVisitor(e)
-  {
+  submitVisitor(e) {
     e.preventDefault()
     this.logInVisitor()
   }
@@ -104,9 +113,12 @@ class Swipe extends ErrorTimeout {
             ref="errorAlert">
             Account not found. Please try again or see the front desk.
           </div>
-          <button className="btn btn-default" type="button" onClick={this.resetSwipe}>
-            <i className="fas fa-redo-alt"></i>&nbsp;
-            Try again</button>
+          <button
+            className="btn btn-default"
+            type="button"
+            onClick={this.resetSwipe}>
+            <i className="fas fa-redo-alt"></i>&nbsp; Try again
+          </button>
         </div>
       )
     } else if (this.state.error === 2) {
@@ -118,23 +130,43 @@ class Swipe extends ErrorTimeout {
           <button className="close" type="button" onClick={this.resetSwipe}>
             <i className="fas fa-times"></i>
           </button>
-          You are already logged in. Please visit the front desk if you have a question or
-          concern.
+          You are already logged in. Please visit the front desk if you have a
+          question or concern.
         </div>
       )
     } else {
-      field = <input
-        id="swiper"
-        type="text"
-        placeholder="Banner ID"
-        onChange={this.handleChange}
-        className="form-control"
-        value={this.state.visitor}/>
-      button = <button
-        className="continue btn btn-default"
-        onClick={this.logInVisitor}>Continue&nbsp;
-        <i className="fa fa-chevron-right fa-sm"></i>
-      </button>
+      field = (
+        <div className="input-group mb-3">
+          <input
+            autoComplete="off"
+            maxLength="9"
+            id="swiper"
+            type="text"
+            placeholder="Banner ID"
+            onChange={(e) => this.handleChange(e.target.value)}
+            className="form-control"
+            value={this.state.visitor}
+          />
+          <div className="input-group-append">
+            <button
+              type="button"
+              className="btn btn-outline-danger"
+              onClick={() => {
+                this.handleChange('')
+              }}>
+              Clear
+            </button>
+          </div>
+        </div>
+      )
+      button = (
+        <button
+          className="continue btn btn-default"
+          onClick={this.logInVisitor}>
+          Continue&nbsp;
+          <i className="fa fa-chevron-right fa-sm"></i>
+        </button>
+      )
     }
 
     return (
@@ -142,12 +174,14 @@ class Swipe extends ErrorTimeout {
         <div className="text-center center-block">
           <p className="title">Welcome! Please Check-in</p>
           <p className="subtitle">Swipe your AppCard to get started</p>
-          <p>Don't have your AppCard?<br/>Enter your Banner ID number instead.</p>
-          <form onSubmit={this.submitVisitor}>
-            {field}
-          </form>
+          <p>
+            Don&apos;t have your AppCard?
+            <br />
+            Enter your Banner ID number instead.
+          </p>
+          <form onSubmit={this.submitVisitor}>{field}</form>
           {button}
-        </div>        
+        </div>
         <div className="clearfix"></div>
       </div>
     )
